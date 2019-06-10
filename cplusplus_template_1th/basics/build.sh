10bd8edde1bb03909fda03a0334e756a3deeac10
gaoxiaoxing <gaoxiaoxing@baidu.com>
2019-06-11 01:21:40 +0800

    modify .gitignore to exclude executable file

--- cplusplus_template_1th/basics/build.sh
+++ cplusplus_template_1th/basics/build.sh
@@ -1,3 +1,3 @@
-g++ -std=c++11 -I ../ max.cpp -o main
-g++ -std=c++11  max_overload.cpp -I ../ -o max_overload
-g++ -std=c++11  max_overload_pointer.cpp -I ../ -o max_overload_pointer
+g++ -std=c++11 -I ../ max.cpp -o main_max
+g++ -std=c++11  max_overload.cpp -I ../ -o main_max_overload
+g++ -std=c++11  max_overload_pointer.cpp -I ../ -o main_max_overload_pointer
