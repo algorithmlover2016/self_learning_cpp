c6fdee7587ba8af271b5d82e4d40d6f06cb1a60e
gaoxiaoxing <gaoxiaoxing@baidu.com>
2019-06-10 20:58:41 +0800

    cmake_learn

--- delete_nullptr.cpp
+++ delete_nullptr.cpp
@@ -11,5 +11,15 @@ void delete_nullptr() {
 int main() {
     std::cout << "hello word" << std::endl;
     delete_nullptr();
+    std::string str("fdsafsadfdsafdsfdsafdsafdsafdasfdsafasdffdfafsdafsfas");
+    std::cout << sizeof(str) << std::endl;
+    std::cout << str.size() << std::endl;
+    std::shared_ptr<int> numInt = std::make_shared<int>(10);
+    std::cout << *numInt << std::endl;
+    std::cout << sizeof(numInt) << std::endl;
+
+    std::shared_ptr<std::string> strPtr(new std::string("fdasfdsf"));
+    std::cout << strPtr->size() << std::endl;
+    std::cout << sizeof(strPtr) << std::endl;
     return 0;
 }
