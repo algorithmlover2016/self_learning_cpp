2667a4e690d80d7beb7f7894c615f72e1d893a2c
gaoxiaoxing <gaoxiaoxing@baidu.com>
2020-01-07 20:46:47 +0800

    update first section in 2th

--- cplusplus_template_2th/basics/max3ref.cpp
+++ cplusplus_template_2th/basics/max3ref.cpp
@@ -1,31 +1,57 @@
 #include <cstring>
+#include <iostream>
+#include <string>
 
 // maximum of two values of any type (call-by-reference)
 template<typename T>
-T const& max (T const& a, T const& b)
-{
+T const & max (T const& a, T const& b) {
+    std::cout << "call template function two values of any type " << __func__ << std::endl;
   return  b < a ? a : b;
 }
 
+// if defining this max overload, then the non-template < char const* max(char const* &a, char const* &b) > will not be called
+// when calling max(s1, s2, s3). And there will be an error[warning] when call max(s1, s2, s3), which is :
+// in instantiation of function template specialization 'max<char>' requested here and
+// reference to stack memory associated with parameter 'a' returned [-Wreturn-stack-address]
+
+// template<typename T>
+// T const * const& max (T const* a, T const* b) {
+//     std::cout << "call template function two values of any type pointer " << __func__ << std::endl;
+//   return  b < a ? a : b;
+// }
+
+/*
+   the upper two overload template functions show that if you define 
+   template<typename T>
+   T const & max (T const& a, T const& b);
+   and 
+   template<typename T>
+   T const * const& max (T const* a, T const* b);
+
+   these two function can't deal with char const * template parameter when recursively call the function.
+*/
+
+// must define the type with reference, otherwise will get reference to stack memory associated with parameter 'b|[a]' returned [-Wreturn-stack-address]
 // maximum of two C-strings (call-by-value)
-char const* max (char const* a, char const* b)
-{
+// But when define with reference, it will never be called
+char const* & max (char const* & a, char const* & b) {
+    std::cout << "call non-template function " << __func__ << std::endl;
   return  std::strcmp(b,a) < 0  ? a : b;
 }
 
 // maximum of three values of any type (call-by-reference)
 template<typename T>
-T const& max (T const& a, T const& b, T const& c)
-{
+T const& max (T const& a, T const& b, T const& c) {
   return max (max(a,b), c);       // error if max(a,b) uses call-by-value
 }
 
-int main ()
-{
+int main () {
   auto m1 = ::max(7, 42, 68);     // OK
+  std::cout << "::max(7, 42, 68) " << m1 << std::endl;
 
   char const* s1 = "frederic";
   char const* s2 = "anica";
   char const* s3 = "lucas";
-  auto m2 = ::max(s1, s2, s3);    // run-time ERROR
+  auto m2 = ::max(s1, s2, s3);    // run-time ERROR || -Wreturn-stack-address if define this char const*  max (char const*  a, char const*  b);
+  std::cout << "::max(s1, s2, s3) " << m2 << std::endl;
 }
--- cplusplus_template_2th/basics/maxdecltypedecay.hpp
+++ cplusplus_template_2th/basics/maxdecltypedecay.hpp
@@ -1,7 +1,11 @@
 #include <type_traits>
 
+/* true ? :
+In the expression true ? std::declval<_Tp>() : std::declval<_Up>() the first alternative is always selected, but the whole expression must be a valid expression. So std::declval<_Up>() must be valid and that means _Up must be a callable that accepts zero arguments. Beside that, _Tp() and _Up() must return the same type (or one of the types must be implicitly convertible to another), otherwise ternary iterator would not be able to select return value.
+
+This technique is called SFINAE (substitution failure is not an error). The idea is that if template instantiation fails, then it is not an error, and this template is just ignored and compiler searches for another one.
+*/
 template<typename T1, typename T2>
-auto max (T1 a, T2 b) -> typename std::decay<decltype(true?a:b)>::type
-{
+auto max (T1 a, T2 b) -> typename std::decay<decltype(true?a:b)>::type {
   return  b < a ? a : b;
 }
