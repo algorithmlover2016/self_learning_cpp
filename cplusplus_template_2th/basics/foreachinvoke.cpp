0d1cef544502da904ca7f30cc28696844facf09b
gaoxiaoxing <gaoxiaoxing@baidu.com>
2020-01-12 13:21:12 +0800

    forearch call template function, how to do that

--- cplusplus_template_2th/basics/foreachinvoke.cpp
+++ cplusplus_template_2th/basics/foreachinvoke.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include "foreachinvoke.hpp"
+// #include "invokeret.hpp"
 
 // a function to call:
 void func(int i) {
@@ -26,6 +27,13 @@ int main()
             std::cout << prefix << i << '\n';
           },
           "- value: ");                          // 1st arg of lambda
+
+  // foreach(primes.begin(), primes.end(),          // elements for 2nd arg of lambda
+  //         call,
+  //         [](std::string const& prefix, int i) { // lambda to call
+  //           std::cout << prefix << i << '\n';
+  //         },
+  //         "--value");
     
   // call obj.memfunc() for/with each elements in primes passed as argument
   MyClass obj;
